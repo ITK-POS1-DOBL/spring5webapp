a4bcb59db32118a9fd648682595f650061815152
Intruder <intruder1912@gmail.com>
2018-06-24 17:29:23 +0200

    added mvc parts for first views (books, authors)...
    first roundtrip from browser view via controller to the model finished...

---
+++ src/main/java/org/doblander/lab/spring5webapp/controllers/BookController.java
@@ -0,0 +1,27 @@
+package org.doblander.lab.spring5webapp.controllers;
+
+import org.doblander.lab.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+
+    @RequestMapping("/books")
+    public String getBooks(Model model){
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        // return the target view name
+        return "books";
+
+    }
+}
